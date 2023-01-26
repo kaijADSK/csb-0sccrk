5b7d1b4c439ab06e4f430b5e7f4b184916a62cde
Code Sandbox <hello@codesandbox.io>
2023-01-26 05:12:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8082a5c98974084e0f526fc03c6cf068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e74db308e5678f6fcead47630f4357",
+    "url": "https://.github.io/csb-0sccrk/index.html"
+  },
+  {
+    "revision": "304e7fb26c0f385a2a49",
+    "url": "https://.github.io/csb-0sccrk/static/css/main.7a02aca6.chunk.css"
+  },
+  {
+    "revision": "d9604790cafc438ac74b",
+    "url": "https://.github.io/csb-0sccrk/static/js/2.fdedbca9.chunk.js"
+  },
+  {
+    "revision": "304e7fb26c0f385a2a49",
+    "url": "https://.github.io/csb-0sccrk/static/js/main.806c454c.chunk.js"
+  },
+  {
+    "revision": "7d1c7f3b70319dfbebe5",
+    "url": "https://.github.io/csb-0sccrk/static/js/runtime~main.5479349f.js"
+  },
+  {
+    "revision": "bb0801d1cc8922c99da1030640509f21",
+    "url": "https://.github.io/csb-0sccrk/static/media/gossip.bb0801d1.mp3"
+  }
+]);
